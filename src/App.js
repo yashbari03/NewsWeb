203b5b5a5b43aab0d1cdfe80d052c94a4f414b30
yashbari03 <bariyash2003@gmail.com>
2024-09-03 21:07:55 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+export default class App extends Component {
+  pageSize = 6;
+
+  state = {
+    progress:0
+  }
+
+  setProgress = (progress)=>{
+    this.setState({progress: progress})
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+        <NavBar/> 
+        <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={this.state.progress} 
+      />
+        <Routes>
+          <Route exact path="/" element={<News setProgress={this.setProgress} key="1" pageSize={this.pageSize} country="sa" category="general"/>} />
+          <Route exact path="/business" element={<News setProgress={this.setProgress} key="business" pageSize={this.pageSize} country="sa" category="business"/>} />
+          <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="sa" category="entertainment"/>} />
+          <Route exact path="/general"  element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="sa" category="general"/>} />
+          <Route exact path="/health" element={<News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="sa" category="health"/>} />
+          <Route exact path="/science" element={<News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="sa" category="science"/>} /> 
+          <Route exact path="/sports" element={<News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="sa" category="sports"/>} />
+          <Route exact path="/technology" element={<News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="sa" category="technology"/>} />
+        </Routes>
+        </Router>
+      </div>
+    )
+  }
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let defaulfImg ="https://media.istockphoto.com/id/1183338499/vector/0547.jpg?s=612x612&w=0&k=20&c=yNkIf4DxCEkOb0EXoq5kQ0XX1k5T53QYQLgL_j2Rg5M="
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }>
+
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? defaulfImg : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
